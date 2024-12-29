f1501c7b8d83b5ed213f8f641e83b7830a663ac8
karmaaazine <salmazine2021@gmail.com>
2024-12-29 22:15:43 +0100

    updates

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,11 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-<<<<<<< HEAD
-    public string $baseURL = 'http://localhost/ci4/';
-=======
     public string $baseURL = 'http://localhost:8080/';
->>>>>>> 399ad472520c71034827e28f9baf4abab57b0981
+
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
@@ -203,10 +200,9 @@ class App extends BaseConfig
      * @see http://www.w3.org/TR/CSP/
      */
     public bool $CSPEnabled = false;
-<<<<<<< HEAD
-=======
+
 
     public $env = 'development'; // Changez cela à 'development'
 
->>>>>>> 399ad472520c71034827e28f9baf4abab57b0981
+
 }
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,13 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-<<<<<<< HEAD
+
 $routes->get('/', 'Home::index');
-=======
+
 
 $routes->get('/', 'Home::index');   
 $routes->get('/registe', 'Home::register');   
 $routes->post('/register', 'RegisterController::store'); 
 
 
->>>>>>> 399ad472520c71034827e28f9baf4abab57b0981
+
